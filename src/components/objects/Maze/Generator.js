13c02c812052778e889dd2020fd0918ba30780dc
Brian Lou <brianlou2002@gmail.com>
2023-12-08 17:53:00 -0500

    Add functional maze generation code

--- src/components/objects/Maze/Generator.js
+++ src/components/objects/Maze/Generator.js
@@ -1,13 +1,15 @@
+import { genRandom, getTiles } from "./pacmangen";
 class Generator{
     constructor(rows, cols){
         this.maze = Array(rows).fill().map(() => Array(cols).fill(0));
     }
+
     generate(){
-        for (let i = 0; i< this.maze.length; i++){
-            for (let j = 0 ;j<this.maze[0].length; j++){
-                this.maze[i][j] = 1;
-            }
-        }
+        genRandom();
+        this.maze = getTiles();
     }
+    
 }
+
+
 export default Generator;
