8588802aed099389555cca2edf842868abfa2c3f
Brian Lou <69982825+brian-lou@users.noreply.github.com>
2023-12-13 05:02:24 -0500

    add comment

--- src/js/constants.js
+++ src/js/constants.js
@@ -1,3 +1,3 @@
 export const WALL_COLOR = 0xfbe9d2;
 export const FLOOR_COLOR = 0xc8baa8;
-export const MOVEMENT_EPS = 0.025;
+export const MOVEMENT_EPS = 0.025; // turning EPS for smoother turns, use smaller value. Scales with movement speed
