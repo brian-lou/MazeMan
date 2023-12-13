8decb10e3a0329bfa9951f62d5e5708ecc877e0f
Bryant Zeng <bz7829@princeton.edu>
2023-12-13 03:44:41 -0500

    new health bars

--- src/js/globalVars.js
+++ src/js/globalVars.js
@@ -1,5 +1,8 @@
 export default {
-  score : 0, // score
-  health : 3, // health
-  items : null// items
-}
+    score: 0, // score
+    health: 3, // health
+    attack: 1, // attack
+    defense: 1, // defense
+    agility: 1, // agility
+    items: null, // itemsnotez
+};
