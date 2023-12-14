c295cd8281df4d401dc14cb3756ea3b54ee7c511
Bryant Zeng <bz7829@princeton.edu>
2023-12-13 23:09:03 -0500

    reduced game screen

--- src/js/pages.js
+++ src/js/pages.js
@@ -1,52 +1,13 @@
 import PAUSE from '../pauseScreen.html';
+import GAME from '../gameScreen.html';
 
 // concept from https://github.com/harveyw24/Glider/blob/main/src/js/pages.js
 
 export function game(document, canvas) {
     document.body.appendChild(canvas);
-
-    let expBar = document.createElement('progress');
-    expBar.id = 'exp';
-    expBar.max = 50; // exp needed to level up
-
-    let level = document.createElement('label');
-    level.id = 'level';
-    level.htmlFor = 'exp';
-
-    let instructions = document.createElement('div');
-    instructions.id = 'instructions';
-    instructions.prepend(expBar);
-    instructions.prepend(level);
-
-    document.body.appendChild(instructions);
-
-    let healthBar = document.createElement('span');
-    healthBar.id = 'health';
-    healthBar.classList.add('attribute');
-    healthBar.innerHTML =
-        '<i id="healthNum" class="fa-solid fa-2xl fa-heart"></i>';
-
-    let atkBar = document.createElement('span');
-    atkBar.id = 'attack';
-    atkBar.classList.add('attribute');
-    atkBar.innerHTML =
-        '<i id="atkNum" class="fa-solid fa-2xl fa-hand-fist"></i>';
-    let defBar = document.createElement('span');
-    defBar.id = 'defense';
-    defBar.classList.add('attribute');
-    defBar.innerHTML = '<i id="defNum" class="fa-solid fa-2xl fa-shield"></i>';
-
-    let items = document.createElement('span');
-    items.id = 'items';
-    items.classList.add('attribute');
-
-    let statBox = document.createElement('div');
-    statBox.id = 'statBox';
-    statBox.append(healthBar);
-    statBox.append(atkBar);
-    statBox.append(defBar);
-    statBox.append(items);
-    document.body.appendChild(statBox);
+    let game = document.createElement('div');
+    game.innerHTML = GAME;
+    document.body.appendChild(game);
 
     let pause = document.createElement('div');
     pause.id = 'pause';
